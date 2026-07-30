a5aabfc94c264821d5a68eaa4ceadb1338338e3b
hernagi <keisuke3312@icloud.com>
2026-07-31 05:12:59 +0900

    v14.7.1

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,8 @@
-const CACHE = "touhan-practice-v14.3.7";
+const CACHE = "touhan-practice-v14.7.1";
 const STATIC_ASSETS = [
-  "./style.css?v=14.3.7",
-  "./app.js?v=14.3.7",
-  "./manifest.json?v=14.3.7",
+  "./style.css?v=14.7.1",
+  "./app.js?v=14.7.1",
+  "./manifest.json?v=14.7.1",
   "./icon.svg",
   "./icon-192.png",
   "./icon-512.png",
